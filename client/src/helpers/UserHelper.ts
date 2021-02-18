19613dd5b22b17a37915606d7ec74bb8adbe05c1
predator <aleksey.kukishev@gmail.com>
2021-02-18 15:32:29 +0200

    remove argon2

--- client/src/helpers/UserHelper.ts
+++ client/src/helpers/UserHelper.ts
@@ -1,5 +1,5 @@
-import argon2 from "argon2";
-
-export function hashString(str: string) {
-	return argon2.hash(str);
-}
+// import argon2 from "argon2";
+export const empty = "";
+// export function hashString(str: string) {
+// 	return argon2.hash(str);
+// }
